b4ec7aff6bc6f8b59d4222835e772e726656e82f
Vladislav Krysin <krysin18@mail.ru>
2021-05-06 10:26:05 +0300

    feat: add process put request on server Put request, which want to add new user

--- server/server.js
+++ server/server.js
@@ -4,9 +4,9 @@ const fs = require('fs')
 
 const hostname = '127.0.0.1'
 const port = 8081;
-
+const serverData = ["users", "comments"]
 // simple version
-function parseRequest(fileName, req, res) {
+function parseGetRequest(fileName, req, res) {
 
   for (item of fileName) {
 
@@ -15,21 +15,50 @@ function parseRequest(fileName, req, res) {
       res.statusCode = 200
       res.setHeader("Content-Type", "application/json")
 
-      fs.readFile(`${item}.json`, (err, data) => {
+      fs.readFile(`live-chat/server/${item}.json`, (err, data) => {
         if (err) throw err
         res.end(JSON.stringify(JSON.parse(data)))
       })
       return
     }
   }
-  res.statusCode = 200
-  res.end(JSON.stringify({ a: 1 }));
 }
 
+// db is json file
+function addUserToDB(user) {
+  const pathToUsers = 'live-chat/server/users.json';
+
+  fs.readFile(pathToUsers, 'utf8', function readFileCallback(err, data){
+      if (err){
+          console.log(err);
+      } else {
+      obj = JSON.parse(data);
+      obj.users.push(user);
+      json = JSON.stringify(obj);
+      fs.writeFile(pathToUsers, json, (err) => {
+        if(err) throw err;
+      });
+  }});
+}
 const server = http.createServer((req, res) => {
 
-  requestData = ["users", "comments"]
-  parseRequest(requestData, req, res)
+  if (req.method == 'GET') {
+    parseGetRequest(serverData, req, res);
+  }
+
+  if (req.method == 'PUT') {
+
+    if (req.url.match(/users/g) !== null) {
+      let user = '';
+      req.on('data', (chunk) => {
+        user += chunk;
+      })
+      req.on('end', () => {
+        addUserToDB(JSON.parse(user).name);
+        res.end()
+      })
+    }
+  }
 
 })
 
